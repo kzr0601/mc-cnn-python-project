22fbe0e985bcff5609a30540e2888f5863cb2350
kzr0601 <3150104116@zju.edu.cn>
2019-05-14 17:54:35 +0800

    bat file for getting results

---
+++ getResult.bat
@@ -0,0 +1,20 @@
+python raw_match.py 145 Adirondack
+python raw_match.py 320 Jadeplant
+python raw_match.py 140 Motorcycle
+python raw_match.py 140 MotorcycleE
+python raw_match.py 130 Piano
+python raw_match.py 130 PianoL
+python raw_match.py 150 Pipes
+python raw_match.py 165 Playroom
+python raw_match.py 145 Playtable
+python raw_match.py 145 PlaytableP
+python raw_match.py 130 Recycle
+python raw_match.py 120 Shelves
+python raw_match.py 128 Teddy
+python raw_match.py 380 Vintage
+
+
+
+
+
+
